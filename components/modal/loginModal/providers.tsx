752ef2ce1c83ed2f674f04c8a737951c0b490880
matehis langlais <contact@matehis.com>
2024-07-28 21:36:34 +0200

    wip

--- components/modal/loginModal/providers.tsx
+++ components/modal/loginModal/providers.tsx
@@ -1,26 +1,13 @@
-import SteamIcon from "@/components/ui/icons/steamIcon";
 import GoogleIcon from "@/components/ui/icons/googleIcon";
 import FacebookIcon from "@/components/ui/icons/facebookIcon";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/hooks/useAuth";
 
 const LoginModalProviders = () => {
-  const { handleGoogleLogin, handleFacebookLogin, handleSteamLogin } =
-    useAuth();
+  const { handleGoogleLogin, handleFacebookLogin } = useAuth();
 
   return (
     <div className="flex-col">
-      <Button
-        size="lg"
-        variant="outline"
-        className="gap-2 w-full mt-3"
-        onClick={handleSteamLogin}
-      >
-        <div className="flex items-center">
-          <SteamIcon />
-          Connexion avec Steam
-        </div>
-      </Button>
       <Button
         size="lg"
         variant="outline"
